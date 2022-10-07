1f35eb71a41f0ac1064bd5663bc1c604485cf13f
Zachary Geier <zgeier@yext.com>
2022-10-07 16:09:09 -0400

    add plugin

---
+++ functions/api/test.ts
@@ -0,0 +1,26 @@
+// Parses arguments from the url query params and creates an entity via Knowledge API
+export async function main(argumentJson) {
+    return {
+        "body": `      
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            </head>
+            <body>
+                <div style=" margin-top: 10%; margin-left: 30%; text-align: center;">
+                    <body>
+                        <h1>Hello, bot!</h1>
+                        <p>You failed to create a new sport, replicant scum.</p>
+                    </body>
+                </div>
+            </body>
+            </html>
+        `,
+        "statusCode": 200,
+        "headers": {
+            "Cache-control": "no-store",
+            "X-Yext-Test": "Example header",
+        },
+      };
+ }
+ 
